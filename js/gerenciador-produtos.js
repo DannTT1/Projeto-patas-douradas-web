be558c6458cf2b87b101f6be739dd59033925481
DannTT1 <matheus.cardoso97@outlook.com>
2025-10-11 07:30:22 -0300

    ..

--- js/gerenciador-produtos.js
+++ js/gerenciador-produtos.js
@@ -4,7 +4,7 @@ const produtosIniciais = [
         id: 1, 
         nome: "Areia Higiênica para Gatos", 
         preco: 29.90, 
-        imagem: "/assets/img/areiahigienicafelina.png", 
+        imagem: "Projeto-patas-douradas-web/assets/img/areiahigienicafelina.png", 
         estoque: 24,
         descricao: "Areia sanitária de alta absorção que forma torrões firmes, facilitando a limpeza e controlando odores."
     },
